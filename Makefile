5299e0edb13fdd3d8735d63a1968e428ea5392ec
Damiano Donati <damiano.donati@gmail.com>
2023-02-05 17:04:42 +0100

    run integration tests on postgres too

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
 .PHONY: all test run run-router7 postgres
 
+export POSTGRES_HOST = 127.0.0.1
+export POSTGRES_PORT = 5433
+export POSTGRES_USER = postgres
+export POSTGRES_PASSWORD = mysecretpassword
+export POSTGRES_DBNAME = postgres
+
 all:
 	go install ./cmd/...
 
 test:
-	go test -v ./...
+	go test -count=1 -v ./...
 
 run: test all
 	gus-server
@@ -13,4 +19,8 @@ run-router7:
 	cd cmd/gus-server && GOARCH=amd64 gok -i router7 run
 
 postgres:
-	docker run --rm -it --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 postgres
+	docker kill gus-postgres || true
+	docker run --rm -it -d --name gus-postgres \
+		-e POSTGRES_USER=$$POSTGRES_USER \
+		-e POSTGRES_PASSWORD=$$POSTGRES_PASSWORD \
+		-p $$POSTGRES_PORT:5432 postgres
--- internal/gusserver/heartbeat_test.go
+++ internal/gusserver/heartbeat_test.go
@@ -3,61 +3,94 @@ package gusserver
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
+type testDatabase struct {
+	databaseType   string
+	databaseSource string
+}
+
+func testDatabases() []testDatabase {
+	pgHost := os.Getenv("POSTGRES_HOST")
+	pgPort := os.Getenv("POSTGRES_PORT")
+	pgUser := os.Getenv("POSTGRES_USER")
+	pgPassword := os.Getenv("POSTGRES_PASSWORD")
+	pgDBName := os.Getenv("POSTGRES_DBNAME")
+
+	dbs := []testDatabase{
+		{"sqlite", ":memory:"},
+		{"postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			pgHost, pgPort, pgUser, pgPassword, pgDBName)},
+	}
+
+	return dbs
+}
+
 func TestHeartbeat(t *testing.T) {
-	srv, mux, err := newServer("sqlite", ":memory:")
-	if err != nil {
-		t.Fatal(err)
-	}
+	testDBs := testDatabases()
 
-	// Ensure the heartbeats table is empty when a fresh server starts
-	rows, err := srv.db.Query("SELECT machine_id FROM heartbeats")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer rows.Close()
-	if rows.Next() {
-		t.Fatalf("heartbeats table unexpectedly contains entries")
-	}
+	for _, tc := range testDBs {
+		t.Run(tc.databaseType, func(t *testing.T) {
+			srv, mux, err := newServer(tc.databaseType, tc.databaseSource)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	testsrv := httptest.NewServer(mux)
-	client := testsrv.Client()
-	b, err := json.Marshal(heartbeatRequest{
-		MachineID: "scan2drive",
-	})
-	req, err := http.NewRequest("POST", testsrv.URL+"/api/v1/heartbeat", bytes.NewReader(b))
-	if err != nil {
-		t.Fatal(err)
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if got, want := resp.StatusCode, http.StatusOK; got != want {
-		t.Fatalf("unexpected HTTP status code: got %v, want %v", resp.Status, want)
-	}
+			if err := srv.db.Ping(); err != nil {
+				t.Fatalf("unable to reach database %s", tc.databaseType)
+			}
 
-	// Ensure the heartbeats table has a corresponding entry now
-	rows, err = srv.db.Query("SELECT machine_id FROM heartbeats")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer rows.Close()
-	if !rows.Next() {
-		t.Fatalf("heartbeats table unexpectedly still contains no entries")
-	}
-	var machineID string
-	if err := rows.Scan(&machineID); err != nil {
-		t.Fatal(err)
-	}
-	if got, want := machineID, "scan2drive"; got != want {
-		t.Fatalf("heartbeats table entry has unexpected machine_id: got %q, want %q", got, want)
-	}
-	if rows.Next() {
-		t.Fatalf("heartbeats table unexpectedly contains more than one entry")
+			// Ensure the heartbeats table is empty when a fresh server starts
+			rows, err := srv.db.Query("SELECT machine_id FROM heartbeats")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer rows.Close()
+			if rows.Next() {
+				t.Fatalf("heartbeats table unexpectedly contains entries")
+			}
+
+			testsrv := httptest.NewServer(mux)
+			client := testsrv.Client()
+			b, err := json.Marshal(heartbeatRequest{
+				MachineID: "scan2drive",
+			})
+			req, err := http.NewRequest("POST", testsrv.URL+"/api/v1/heartbeat", bytes.NewReader(b))
+			if err != nil {
+				t.Fatal(err)
+			}
+			resp, err := client.Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got, want := resp.StatusCode, http.StatusOK; got != want {
+				t.Fatalf("unexpected HTTP status code: got %v, want %v", resp.Status, want)
+			}
+
+			// Ensure the heartbeats table has a corresponding entry now
+			rows, err = srv.db.Query("SELECT machine_id FROM heartbeats")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer rows.Close()
+			if !rows.Next() {
+				t.Fatalf("heartbeats table unexpectedly still contains no entries")
+			}
+			var machineID string
+			if err := rows.Scan(&machineID); err != nil {
+				t.Fatal(err)
+			}
+			if got, want := machineID, "scan2drive"; got != want {
+				t.Fatalf("heartbeats table entry has unexpected machine_id: got %q, want %q", got, want)
+			}
+			if rows.Next() {
+				t.Fatalf("heartbeats table unexpectedly contains more than one entry")
+			}
+		})
 	}
 }
